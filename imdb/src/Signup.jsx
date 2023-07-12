bc263b4d5d36a2e9fefdf39a16f725800e41a6e1
Imam sultan <imamsultan47@gmail.com>
2023-07-12 11:07:20 +0530

    first commit

--- imdb/src/Signup.jsx
+++ imdb/src/Signup.jsx
@@ -1,36 +1,38 @@
 import { useState } from "react";
-import 'bootstrap/dist/css/bootstrap.min.css'
-import axios from 'axios'
-import {Link, useNavigate} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import { Link, useNavigate } from 'react-router-dom';
 
+const reg = process.env.REGISTER
 function Signup() {
-    const [name, setName] = useState()
-    const [email, setEmail] = useState()
-    const [password, setPassword] = useState()
-    const navigate = useNavigate()
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        axios.post("http://localhost:3001/register", {name, email, password})
-        .then(res => {
-            navigate('/login')
-        }).catch(err => console.log(err))
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    axios.post(reg/*'http://localhost:3001/register'*/, { name, email, password })
+      .then(res => {
+        navigate('/login');
+      })
+      .catch(err => console.log(err));
+  };
 
   return (
     <div className="d-flex justify-content-center align-items-center bg-black vh-100">
       <div className="bg-white p-3 rounded w-25">
         <h2>Register</h2>
         <form onSubmit={handleSubmit}>
-        <div className="mb-3">
-            <label htmlFor="email">
+          <div className="mb-3">
+            <label htmlFor="name">
               <strong>Name</strong>
             </label>
             <input
               type="text"
               placeholder="Enter Name"
               autoComplete="off"
-              name="email"
+              name="name"
               className="form-control rounded-0"
               onChange={(e) => setName(e.target.value)}
             />
@@ -49,7 +51,7 @@ function Signup() {
             />
           </div>
           <div className="mb-3">
-            <label htmlFor="email">
+            <label htmlFor="password">
               <strong>Password</strong>
             </label>
             <input
@@ -63,15 +65,13 @@ function Signup() {
           <button type="submit" className="btn btn-success w-100 rounded-0">
             Register
           </button>
-          </form>
-          <p>Already Have an Account</p>
-          <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
-            Login
-          </Link>
-        
+        </form>
+        <p>Already Have an Account</p>
+        <Link to="/login" className="btn btn-default border w-100 bg-light rounded-0 text-decoration-none">
+          Login
+        </Link>
       </div>
     </div>
   );
 }
-
 export default Signup;
